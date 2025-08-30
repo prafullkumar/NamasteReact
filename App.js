659178428ef0ec14da50d5bb1eb31d009b3b4ea3
Prafull Tupe <kumartupe@gmail.com>
2025-08-30 16:12:57 +0530

    pushing code from ep1

---
+++ App.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-undef 
+
+<div  id="parent">
+      <div id="child">
+         <h1 id="heading">Hello World</h1>
+         <h2>This is h2 tag</h2>
+         </div>
+
+         <div id="child2">
+         <h1>This is h1 tag</h1>
+         <h2>This is h2 tag</h2>
+         </div>
+</div>
+
+*/
+
+const parent = React.createElement(
+  "div",
+  {
+    id: "parent",
+  },
+  [
+    React.createElement(
+      "div",
+      {
+        id: "child",
+      },
+      [
+        React.createElement("h1", {}, "Hello World"),
+        React.createElement("h1", {}, "Hello World2"),
+      ]
+    ),
+    React.createElement(
+      "div",
+      {
+        id: "child2",
+      },
+      [
+        React.createElement("h1", {}, "Hello World childe2"),
+        React.createElement("h1", {}, "Hello World2 chiled2"),
+      ]
+    ),
+  ]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(parent);
+root.render(parent);
